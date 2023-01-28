bea3808b441bf30c2e69afcd0f47b35331b606a5
sabbir1257 <masabbir1257@gmail.com>
2023-01-28 16:28:46 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,30 @@
+// number type variable 
+var sunglassprice = 120;
+
+// string type variable
+var specialOneParsion = 'mim akter';
+
+// boolean type variable 
+var isSingle = true; 
+
+// two types of number: Integer: 120, 
+// float, decimal: 3.65, 
+
+// variaable naming convention: rules, best practice
+/**
+ * 1. single word
+ * 2. no gap or speace
+ * 3. no quyote
+ * 4. no keyword
+ * 5. con not start with a number
+ * 6. can not contain dash
+ * 7. should use camelCase
+**/ 
+
+
+// Math Operations : +, -. *, /,
+// Shorthand : +=, -=, ++, --,
+
+// convert : parseInt, parsefloat
+
+// tofixed : remainder : VAGSES : % 
