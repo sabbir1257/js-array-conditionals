7b2b49c168c6226a09f4955cf855dc36266a1402
sabbir1257 <masabbir1257@gmail.com>
2023-01-29 14:17:05 +0600

    push

--- array.js
+++ array.js
@@ -1,4 +1,5 @@
 var freandsAge = [12, 15, 14, 15, 17];
 var picnicfee = [500, 200, 450, 700];
 var freandsName = ['raidul', 'saymoon', 'emon']
+var vowels = ['a', 'e', 'i', 'o', 'u' ]
 console.log(freandsName.length);
---
+++ index.js
@@ -0,0 +1,14 @@
+var numbers = [56, 48, 48, 12, 85, 95]
+
+//1. get element by index
+var elemant = numbers[1];
+// console.log(elemant);
+
+//2. set element value by index 
+numbers[1]= 77;
+numbers[3] =44;
+// console.log(numbers);
+
+// 3. find index of an element
+var positionIndex = numbers.indexOf(85);
+console.log(positionIndex);
