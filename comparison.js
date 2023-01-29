28d9ce46f637fcb6e7fe86b545a3f3d29c74d03b
sabbir1257 <masabbir1257@gmail.com>
2023-01-29 15:44:57 +0600

    push

---
+++ comparison.js
@@ -0,0 +1,12 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(6 == 6)
+// console.log(5 ==6);
+// console.log(6 != 5);
+// console.log(15 != 15);
+
+
+// less than or equal
+// console.log(5 <= 6); 
+// console.log(6 <= 6);
+console.log(16 <= 6);
---
+++ push-pop.js
@@ -0,0 +1,15 @@
+var numbers = [45, 52, 15, 68];
+// use push to add element to an array as the last element array
+// numbers.push(55);
+// console.log(numbers);
+
+
+var freands =['raidul', 'symoon', 'emon']
+freands.push('usman');
+freands.push('fahime')
+// console.log(freands);
+
+
+// use pop to get last element
+numbers.pop();
+console.log(numbers);
