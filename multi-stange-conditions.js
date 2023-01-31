01e2265986c7879f2639577be409ce3a396bb87b
sabbir1257 <masabbir1257@gmail.com>
2023-01-31 14:46:11 +0600

    push

---
+++ multi-stange-conditions.js
@@ -0,0 +1,17 @@
+var money = 10;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if (danishPrice < money) {
+    console.log('Dan Dan danish Khamu.');
+}
+else if (butterBread < money) {
+    console.log('Butter bon khamu.');
+}
+else if (toastBiscuit < money) {
+    console.log('Toast khamu');
+}
+else{
+    console.log('khslie cha');
+}
