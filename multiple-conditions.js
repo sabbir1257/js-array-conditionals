5f76ce3ec537a4769ddf24d0b678584c258e5757
sabbir1257 <masabbir1257@gmail.com>
2023-01-31 14:30:31 +0600

    push

--- multiple-conditions.js
+++ multiple-conditions.js
@@ -1,6 +1,6 @@
-var isGraduated = true;
-var salary = 75000;
-var cars = 1;
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 1;
 
 // if (isGraduated == true){
 //     console.log('Eso biye kore feli')
@@ -16,9 +16,22 @@ var cars = 1;
 //     console.log('tor kopale Biya nai')
 // }
 
-if (isGraduated == true && salary > 50000 && cars >= 1){
-    console.log('Eso biye kore feli')
+// if (isGraduated == true && salary > 50000 && cars >= 1){
+//     console.log('Eso biye kore feli')
+// }
+// else{
+//     console.log('tor kopale Biya nai')
+// }
+
+
+
+var isGraduated = false;
+var salary = 35000;
+var cars = 1;
+
+if (isGraduated === true || salary > 50000 || cars >= 1){
+    console.log('Eso Prim kore')
 }
 else{
-    console.log('tor kopale Biya nai')
+    console.log('tomar friend air Number ta dao' )
 }
